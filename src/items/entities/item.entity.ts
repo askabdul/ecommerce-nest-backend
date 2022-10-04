8e1a7fac0018e05baf6a19b4b8d3b10d4bcb364a
salam <azizsalam148@gmail.com>
2022-10-04 03:04:16 +0000

    adding typeorm entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,9 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Item {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   details: string;
+
+  @Column()
   quantity: number;
+
+  @Column()
   price: number;
+
+  @Column()
   status: string;
+
+  @Column('json', { nullable: true })
   images: string[];
 }
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
-@Module({ controllers: [ItemsController], providers: [ItemsService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Item])],
+  controllers: [ItemsController],
+  providers: [ItemsService],
+})
 export class ItemsModule {}
